73504f8117a3cf7ca790e4d313cc911e151af930
Trey Wenger <tvwenger@gmail.com>
2015-02-04 14:34:21 -0500

    updated to 1.0.1

--- FindCompactGroups.py
+++ FindCompactGroups.py
@@ -2,6 +2,9 @@
 FindCompactGroups.py
 Search a file from the mini-millenium survey for compact groups
 v1.0.0 - Trey Wenger - 20 January 2015
+v1.0.1 - Trey Wenger - 04 February 2015
+         Added progress indicators
+         Re-calculate separation ratio after throwing out bad groups
 """
 vers = "v1.0.0"
 
@@ -9,6 +12,7 @@ import sys
 import argparse
 import numpy as np
 from sklearn.cluster import MeanShift
+from sklearn.neighbors import NearestNeighbors
 
 #=====================================================================
 # Compact Group Object
@@ -23,6 +27,7 @@ class CompactGroup:
         """
         self.label = label
         self.members = members
+        self.sep_ratio = None
 
     def eliminateDwarfs(self, min_range=3):
         """
@@ -50,21 +55,33 @@ class CompactGroup:
         """
         Calculate radius of cluster
         """
-        dists = np.sqrt((self.members['x']-self.mediod["x"])**2.+
-                        (self.members['y']-self.mediod["y"])**2.+
-                        (self.members['z']-self.mediod["z"])**2.)
-        self.radius = np.max(dists)
+        dists = (self.members['x']-self.mediod["x"])**2.+\
+                (self.members['y']-self.mediod["y"])**2.+\
+                (self.members['z']-self.mediod["z"])**2.
+        self.radius = np.sqrt(np.max(dists))
 
-    def calculateSeparationRatio(self,groups):
-        """
-        Calculate the separation ratio between this and other groups
-        """
-        dists = np.array([np.sqrt((cg.mediod["x"] - self.mediod["x"])**2.+
-                                  (cg.mediod["y"] - self.mediod["y"])**2.+
-                                  (cg.mediod["z"] - self.mediod["z"])**2.)
-                          for cg in groups if cg != self])
-        min_sep = np.min(dists)
-        self.sep_ratio = self.radius/min_sep
+#=====================================================================
+# Calculate Separation Ratio
+#=====================================================================
+def calcSepRatio(groups):
+    """
+    Calculate separation ratio for each group
+    """
+    # locations of groups
+    locs = [[cg.mediod["x"],cg.mediod["y"],cg.mediod["z"]]
+            for cg in groups]
+    neighbors = NearestNeighbors(n_neighbors=2,algorithm='ball_tree')
+    neighbors.fit(locs)
+    for c_ind,cg in enumerate(groups):
+        sys.stdout.write("\r[{0:10s}] {1}%".\
+                         format('#'*(10*(1+c_ind)/len(groups)),
+                                100*(1+c_ind)/len(groups)))
+        closest = neighbors.kneighbors([cg.mediod["x"],
+                                        cg.mediod["y"],
+                                        cg.mediod["z"]])
+        # neighbor[0] is me, neighbor[1] is actual neighbor
+        dist = closest[0][0][1]
+        cg.sep_ratio = cg.radius/dist
 
 #=====================================================================
 # Find Groups Algorithm
@@ -76,14 +93,17 @@ def main(filename,bandwidth=0.1,min_members=3,max_sep_ratio=1.0,
     """
     Search filename for compact groups
     """
+    print "Reading in data file..."
     # read in data file
     data = np.genfromtxt(filename,dtype=None,delimiter=',',
                          names=True,comments="#")
+    print "Found {0} galaxies in data file...".format(len(data))
     # set up mean-shift algorithm
     ms = MeanShift(bandwidth=bandwidth,min_bin_freq=min_members,
                    cluster_all=False)
     # perform the fit
     X = np.array(zip(data['x'],data['y'],data['z']))
+    print "Locating galaxy groups..."
     ms.fit(X)
     labels = ms.labels_
     # get unique labels
@@ -91,11 +111,16 @@ def main(filename,bandwidth=0.1,min_members=3,max_sep_ratio=1.0,
     n_clusters = len(labels_unique)
 
     # create cluster object for each cluster
+    print "Found {0} potential groups...".format(n_clusters)
+    print "Measuring group properties..."
     groups = []
-    for label in labels_unique:
+    for l_ind,label in enumerate(labels_unique):
         # skip label -1 (ungrouped)
         if label == -1:
             continue
+        sys.stdout.write("\r[{0:10s}] {1}%".\
+                         format('#'*(10*(1+l_ind)/n_clusters),
+                                100*(1+l_ind)/n_clusters))
         # get all members of this group
         members_ind = [ind for (ind,this_label) in enumerate(labels)
                        if this_label == label]
@@ -110,18 +135,34 @@ def main(filename,bandwidth=0.1,min_members=3,max_sep_ratio=1.0,
         cg.calculateRadius()
         # add it to the list
         groups.append(cg)
+    print
 
     # calculate separation ratio for each group, and pull out
     # good groups
+    print "Calculating separation ratio of groups..."
     good_groups = []
+    calcSepRatio(groups)
+    print
+    print "Eliminating groups with"
+    print "-- separation ratio > {0}".format(max_sep_ratio)
+    print "-- less than {0} members".format(min_members)
     for cg in groups:
-        cg.calculateSeparationRatio(groups)
         if cg.sep_ratio < max_sep_ratio and len(cg.members) > min_members:
             good_groups.append(cg)
 
     print "Found {0} good groups.".format(len(good_groups))
 
+    # re-calculate separation ratio for each group
+    if len(good_groups) > 0:
+        print "Re-calculating separation ratio of good groups..."
+        calcSepRatio(good_groups)
+        print
+    else:
+        print "No good groups!"
+        return
+
     # save group statistics
+    print "Saving groups..."
     with open(groupfile,'w') as f:
         f.write("group_id,x,y,z,radius,sep_ratio,num_members\n")
         for cg in good_groups:
@@ -131,6 +172,7 @@ def main(filename,bandwidth=0.1,min_members=3,max_sep_ratio=1.0,
                            len(cg.members)))
 
     # save galaxy statistics
+    print "Saving group members..."
     with open(memberfile,'w') as f:
         f.write("group_id,member_id,x,y,z,mag_r\n")
         for cg in good_groups:
